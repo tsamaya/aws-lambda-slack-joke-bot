224309943edd1b4838d42919fc543664095ae295
Arnaud Ferrand <arferrand@gmail.com>
2022-02-15 10:40:29 +0000

    move to slack

--- src/functions/bot/handler.ts
+++ src/functions/bot/handler.ts
@@ -2,13 +2,13 @@ import type { APIGatewayProxyHandler } from 'aws-lambda';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { getDadJoke } from '@libs/jokes/fetch-joke';
 import { Joke } from '@libs/jokes/joke';
-import { buildTweet, sendTweet } from '@libs/twitter/tweet';
+import { SlackMessage, buildSlackMessage, postSlackMessage } from '@libs/slack';
 
 const bot: APIGatewayProxyHandler = async () => {
   try {
     const dadJoke: Joke = await getDadJoke();
-    const tweet = buildTweet(dadJoke.joke);
-    await sendTweet(tweet);
+    const message: SlackMessage = buildSlackMessage(dadJoke.joke);
+    await postSlackMessage(message);
     return formatJSONResponse({
       message: 'Hey mate, check your feed!',
     });
